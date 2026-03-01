a6cd4dbfb5bb6aca285c919bd3133eb278efccc6
gk-936 <ggokul0273@gmail.com>
2026-03-02 03:19:15 +0530

    Add initial application and data entity classes

---
+++ app/src/main/java/com/dearmoon/shield/ShieldApplication.java
@@ -0,0 +1,8 @@
+package com.dearmoon.shield;
+
+import android.app.Application;
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class ShieldApplication extends Application {
+}
